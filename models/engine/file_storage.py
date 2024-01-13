adc4f1595b90af4b0db859a437c36ed4d41e2369
precious-appiah <appiahprecious99@gmail.co>
2024-01-13 22:16:12 +0000

    feat: storage feat

---
+++ models/engine/file_storage.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""This is our storage handling module"""
+import json
+import os
+
+
+class FileStorage():
+    """Handles json conversion"""
+    __path = 'storage_file.json'
+    __objects = {}
+
+    def all(self):
+        """Returns objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """adds new object in the dictionary"""
+        self.__objects[str(type(obj).__name__) + '.' + obj.id] = obj.__dict__
+
+    def save(self):
+        """serializes our objects dict to a json str"""
+        with open(self.__path, 'w', encoding="utf-8") as f:
+            json.dump(self.__objects, f, indent=2)
+
+    def reload(self):
+        """Reloads objects from json file"""
+        if os.path.isfile(self.__path):
+            with open(self.__path, 'r') as f:
+                self.__objects = json.load(f)
+                return self.__objects
