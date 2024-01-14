54c415e71940e6fc4ec9d614a694db01e7d85dc2
Francis Thore <thorefrancis@gmail.com>
2024-01-14 22:29:29 +0200

    fix: undo datetime stuff

--- models/base_model.py
+++ models/base_model.py
@@ -26,8 +26,6 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now().isoformat()
-            self.created_at = datetime.strptime(self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.strptime(self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
             storage.new(self)
 
     def save(self):
